b117b4498fb032e38c6efd940db5fda28ebf94bd
Brian Ly <brian.kyang.ly@gmail.com>
2019-10-11 14:37:13 -0700

    Brian and Kara

--- src/index.js
+++ src/index.js
@@ -1,18 +1,85 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+URL = "http://localhost:3000/toys";
+const toyCollection = document.getElementById("toy-collection");
 
-// YOUR CODE HERE
+document.addEventListener("DOMContentLoaded", () => {
+  createToy();
+  fetchToys();
+});
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
+const addBtn = document.querySelector("#new-toy-btn");
+const toyForm = document.querySelector(".container");
+let addToy = false;
+
+addBtn.addEventListener("click", () => {
+  addToy = !addToy;
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = "block";
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = "none";
   }
-})
+});
 
+function createToy() {
+  const createToyForm = document.getElementsByClassName("add-toy-form")[0];
+  createToyForm.addEventListener("submit", event => {
+    event.preventDefault();
+    let name = event.target.elements["name"].value;
+    let image = event.target.elements["image"].value;
 
-// OR HERE!
+    let config = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        image: image
+      })
+    };
+
+    fetch(URL, config)
+      .then(res => res.json())
+      .then(toy => appendToy(toy));
+  });
+}
+
+function appendToy(toy) {
+  const toyCard = document.createElement("div");
+  toyCard.className = "card";
+
+  const toyName = document.createElement("h2");
+  toyName.innerText = toy.name;
+
+  const toyImage = document.createElement("img");
+  toyImage.className = "toy-avatar";
+  toyImage.setAttribute("src", toy.image);
+
+  const toyLikes = document.createElement("p");
+  toyLikes.innerText = `${toy.likes} Likes`;
+
+  const likeButton = document.createElement("button");
+  likeButton.className = "like-btn";
+
+  likeButton.innerHTML = "&#128077";
+  likeButton.addEventListener("click", () => {
+    toy.likes += 1;
+    toyLikes.innerText = `${toy.likes} Likes`;
+  });
+
+  toyCard.append(toyName, toyImage, toyLikes, likeButton);
+  toyCollection.append(toyCard);
+}
+
+function fetchToys() {
+  fetch(URL)
+    .then(res => res.json())
+    .then(json => {
+      renderToy(json);
+    });
+}
+
+function renderToy(json) {
+  for (const toy of json) {
+    appendToy(toy);
+  }
+}
